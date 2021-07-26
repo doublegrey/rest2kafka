8f9f548d027a1d0cb06c271f78596714d19c9380
doublegrey <sergejkropinov@gmail.com>
2021-07-26 11:52:08 +0300

    produce message to kafka

---
+++ main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+	"github.com/gin-gonic/gin"
+)
+
+type Request struct {
+	Key   string      `json:"key"`
+	Value interface{} `json:"value"`
+}
+
+func main() {
+	p, err := kafka.NewProducer(&kafka.ConfigMap{"bootstrap.servers": os.Getenv("BROKER")})
+	if err != nil {
+		log.Fatal("Failed to create producer:", err)
+	}
+	r := gin.Default()
+	authorized := r.Group("/", gin.BasicAuth(gin.Accounts{
+		"admin": "admin",
+		"orch":  "rest2kafka",
+	}))
+	authorized.POST("/:topic", func(c *gin.Context) {
+		var request Request
+		err = c.ShouldBindJSON(&request)
+		if err != nil {
+			log.Printf("Failed to unmarshal request body: %v", err)
+			c.Status(http.StatusBadRequest)
+			return
+		}
+		topic := c.Param("topic")
+		deliveryChan := make(chan kafka.Event)
+		value, err := json.Marshal(request.Value)
+		if err != nil {
+			log.Printf("Failed to marshal value: %v", err)
+			c.Status(http.StatusBadRequest)
+			return
+		}
+		err = p.Produce(&kafka.Message{
+			TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
+			Value:          value,
+			Key:            []byte(request.Key),
+		}, deliveryChan)
+		e := <-deliveryChan
+		m := e.(*kafka.Message)
+
+		if err != nil || m.TopicPartition.Error != nil {
+			log.Printf("Failed to produce message: %v\n", m.TopicPartition.Error)
+		}
+		close(deliveryChan)
+
+		c.JSON(200, gin.H{"Partition": m.TopicPartition.Partition, "Offset": m.TopicPartition.Offset})
+	})
+	r.Run(fmt.Sprintf(":%s", os.Getenv("PORT")))
+}
