b9d51523bbf8fa6776d7f562d67b43a79bfe088c
doublegrey <sergejkropinov@gmail.com>
2021-08-04 10:11:30 +0300

    auth and mongo

--- go.mod
+++ go.mod
@@ -8,10 +8,12 @@ require (
 	github.com/go-playground/validator/v10 v10.8.0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/json-iterator/go v1.1.11 // indirect
+	github.com/linkedin/goavro v2.1.0+incompatible // indirect
 	github.com/mattn/go-isatty v0.0.13 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
 	github.com/ugorji/go v1.2.6 // indirect
+	go.mongodb.org/mongo-driver v1.7.0 // indirect
 	google.golang.org/protobuf v1.27.1 // indirect
 	gopkg.in/confluentinc/confluent-kafka-go.v1 v1.7.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
--- main.go
+++ main.go
@@ -1,19 +1,33 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"strings"
+	"time"
 
 	"github.com/confluentinc/confluent-kafka-go/kafka"
 	"github.com/gin-gonic/gin"
+	"github.com/linkedin/goavro"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type Request struct {
 	Key   string      `json:"key"`
 	Value interface{} `json:"value"`
+}
+
+type TopicSchema struct {
+	Created time.Time `json:"created"`
+	Name    string    `json:"name"`
+	Topic   string    `json:"topic"`
+	Data    string    `json:"data"`
 }
 
 func main() {
@@ -23,11 +37,37 @@ func main() {
 	}
 	r := gin.Default()
 
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb://admin:welcome%4031415@20.52.39.84:27017/?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Disconnect(ctx)
+
 	r.POST("/:topic", func(c *gin.Context) {
-		if c.GetHeader("Authorization") != os.Getenv("TOKEN") {
-			c.Status(http.StatusUnauthorized)
-			return
+		var schemas []TopicSchema
+		cursor, _ := client.Database("orch").Collection("schemas").Find(context.TODO(), bson.M{"topic": c.Param("topic")})
+		if strings.ToLower(os.Getenv("AUTH")) == "true" {
+			count, _ := client.Database("orch").Collection("users").CountDocuments(context.TODO(), bson.M{"token": c.GetHeader("Authorization")})
+			if count == 0 {
+				c.Status(http.StatusUnauthorized)
+				return
+			}
 		}
+
+		err = cursor.All(context.TODO(), &schemas)
+		var latest TopicSchema
+		for _, schema := range schemas {
+			if schema.Created.After(latest.Created) {
+				latest = schema
+			}
+		}
+		valid := false
+
 		var request Request
 		err = c.ShouldBindJSON(&request)
 		if err != nil {
@@ -35,28 +75,45 @@ func main() {
 			c.Status(http.StatusBadRequest)
 			return
 		}
-		topic := c.Param("topic")
-		deliveryChan := make(chan kafka.Event)
-		value, err := json.Marshal(request.Value)
-		if err != nil {
-			log.Printf("Failed to marshal value: %v", err)
-			c.Status(http.StatusBadRequest)
+
+		bytes, _ := json.Marshal(request.Value)
+
+		if len(latest.Data) > 0 {
+			codec, _ := goavro.NewCodec(latest.Data)
+
+			_, _, err = codec.NativeFromTextual(bytes)
+			if err == nil {
+				valid = true
+			}
+		} else {
+			valid = true
+		}
+
+		if valid {
+			topic := c.Param("topic")
+			deliveryChan := make(chan kafka.Event)
+			value, err := json.Marshal(request.Value)
+			if err != nil {
+				log.Printf("Failed to marshal value: %v", err)
+				c.Status(http.StatusBadRequest)
+				return
+			}
+			err = p.Produce(&kafka.Message{
+				TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
+				Value:          value,
+				Key:            []byte(request.Key),
+			}, deliveryChan)
+			e := <-deliveryChan
+			m := e.(*kafka.Message)
+
+			if err != nil || m.TopicPartition.Error != nil {
+				log.Printf("Failed to produce message: %v\n", m.TopicPartition.Error)
+			}
+			close(deliveryChan)
+			c.JSON(200, gin.H{"Partition": m.TopicPartition.Partition, "Offset": m.TopicPartition.Offset})
 			return
 		}
-		err = p.Produce(&kafka.Message{
-			TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
-			Value:          value,
-			Key:            []byte(request.Key),
-		}, deliveryChan)
-		e := <-deliveryChan
-		m := e.(*kafka.Message)
-
-		if err != nil || m.TopicPartition.Error != nil {
-			log.Printf("Failed to produce message: %v\n", m.TopicPartition.Error)
-		}
-		close(deliveryChan)
-
-		c.JSON(200, gin.H{"Partition": m.TopicPartition.Partition, "Offset": m.TopicPartition.Offset})
+		c.Status(http.StatusBadRequest)
 	})
 	r.Run(fmt.Sprintf(":%s", os.Getenv("PORT")))
 }
