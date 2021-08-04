27be1cd7c6242cd2e01b8605c427272017e713e9
doublegrey <sergejkropinov@gmail.com>
2021-08-04 10:50:45 +0300

    consumer

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/gin-gonic/gin v1.7.2 // indirect
 	github.com/go-playground/validator/v10 v10.8.0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/json-iterator/go v1.1.11 // indirect
 	github.com/linkedin/goavro v2.1.0+incompatible // indirect
 	github.com/mattn/go-isatty v0.0.13 // indirect
--- main.go
+++ main.go
@@ -5,13 +5,16 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
 	"time"
 
 	"github.com/confluentinc/confluent-kafka-go/kafka"
 	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
 	"github.com/linkedin/goavro"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -28,6 +31,18 @@ type TopicSchema struct {
 	Name    string    `json:"name"`
 	Topic   string    `json:"topic"`
 	Data    string    `json:"data"`
+}
+type TopicMessage struct {
+	Offset    int64     `json:"offset"`
+	Key       string    `json:"key"`
+	Value     string    `json:"value"`
+	Timestamp time.Time `json:"timestamp"`
+}
+
+var upGrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
 }
 
 func main() {
@@ -47,6 +62,67 @@ func main() {
 		log.Fatal(err)
 	}
 	defer client.Disconnect(ctx)
+
+	r.GET("/:topic", func(c *gin.Context) {
+		ws, _ := upGrader.Upgrade(c.Writer, c.Request, nil)
+		defer ws.Close()
+		mt, token, _ := ws.ReadMessage()
+
+		if strings.ToLower(os.Getenv("AUTH")) == "true" {
+			count, _ := client.Database("orch").Collection("users").CountDocuments(context.TODO(), bson.M{"token": string(token)})
+			if count == 0 {
+				c.Status(http.StatusUnauthorized)
+				return
+			}
+		}
+		ctx, cancel := context.WithCancel(context.TODO())
+
+		run := true
+		go func() {
+			for run {
+				_, b, err := ws.ReadMessage()
+				if err != nil {
+					run = false
+					cancel()
+				}
+				if string(b) == "stop" {
+					run = false
+					cancel()
+				}
+			}
+		}()
+
+		consumer, err := kafka.NewConsumer(&kafka.ConfigMap{
+			"bootstrap.servers":               os.Getenv("BROKER"),
+			"group.id":                        "rest2kafka_" + strconv.Itoa(rand.Intn(10000)),
+			"session.timeout.ms":              6000,
+			"go.events.channel.enable":        true,
+			"enable.auto.commit":              false,
+			"go.application.rebalance.enable": true,
+			"enable.partition.eof":            true,
+			"auto.offset.reset":               "earliest"})
+		if err != nil {
+			fmt.Println(err)
+		}
+		consumer.SubscribeTopics([]string{c.Param("topic")}, nil)
+		for run {
+			select {
+			case <-ctx.Done():
+				run = false
+			case ev := <-consumer.Events():
+				switch e := ev.(type) {
+				case kafka.AssignedPartitions:
+					consumer.Assign(e.Partitions)
+				case kafka.RevokedPartitions:
+					consumer.Unassign()
+				case *kafka.Message:
+
+					b, _ := json.Marshal(TopicMessage{Offset: int64(e.TopicPartition.Offset), Key: string(e.Key), Value: string(e.Value), Timestamp: e.Timestamp})
+					ws.WriteMessage(mt, b)
+				}
+			}
+		}
+	})
 
 	r.POST("/:topic", func(c *gin.Context) {
 		var schemas []TopicSchema
